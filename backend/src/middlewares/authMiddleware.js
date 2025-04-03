32010dfba30a7aff5f96b947e6fe7bb9259496d4
kajolsinghme <kajolsingh985@gmail.com>
2025-04-04 01:33:53 +0530

    Added authorisedRolesMiddleware for role-based access control

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -1,60 +1,95 @@
-import { validatorRegisterUser, validatorLoginUser } from "../validators/authValidator.js";
-import {fromZodError} from "zod-validation-error"
+import {
+  validatorRegisterUser,
+  validatorLoginUser,
+} from "../validators/authValidator.js";
+import { fromZodError } from "zod-validation-error";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 export const validateRegisterUserMiddleware = (req, res, next) => {
-    const validationResult = validatorRegisterUser.safeParse(req.body);
+  const validationResult = validatorRegisterUser.safeParse(req.body);
 
-    if (!validationResult.success) {
-        const formattedError = fromZodError(validationResult.error)
-        return res.status(400).json({
-            success: false,
-            message: formattedError.message,
-        });
-    }
-    else{
-        next();
-    }
+  if (!validationResult.success) {
+    const formattedError = fromZodError(validationResult.error);
+    return res.status(400).json({
+      success: false,
+      message: formattedError.message,
+    });
+  } else {
+    next();
+  }
 };
 
 export const validateLoginUserMiddleware = (req, res, next) => {
-    const validationResult = validatorLoginUser.safeParse(req.body);
+  const validationResult = validatorLoginUser.safeParse(req.body);
 
-    if(!validationResult.success){
-        const formattedError = fromZodError(validationResult.error)
-        return res.status(400).json({
-            success: false,
-            message: formattedError.message,
+  if (!validationResult.success) {
+    const formattedError = fromZodError(validationResult.error);
+    return res.status(400).json({
+      success: false,
+      message: formattedError.message,
+    });
+  } else {
+    next();
+  }
+};
+
+export const validateAuthenticationMiddleware = async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await User.findById(decoded.userId);
+
+      if (!req.user) {
+        return res
+          .status(401)
+          .json({ success: false, message: "User not found" });
+      }
+
+      next();
+    } catch (error) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Unauthorized - Invalid Token" });
+    }
+  } else {
+    return res
+      .status(401)
+      .json({ success: false, message: "No token provided" });
+  }
+};
+
+export const authorisedRolesMiddleware = (allowedRoles) => (req, res, next) => {
+  try {
+    if (!req.user || !req.user.role) {
+      return res
+        .status(403)
+        .json({ success: false, message: "Access denied. Role not found." });
+    }
+
+    if (!allowedRoles.includes(req.user.role)) {
+      return res
+        .status(403)
+        .json({
+          success: false,
+          message: "Access denied. Insufficient permissions",
         });
     }
-    else{
-        next();
-    }
-}
 
-export const validateAuthenticationMiddleware = async(req, res, next) => {
-
-    let token
-
-    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
-        try{
-            token = req.headers.authorization.split(" ")[1]
-            const decoded = jwt.verify(token, process.env.JWT_SECRET)
-
-            req.user =  await User.findById(decoded.userId)
-
-            if (!req.user) {
-                return res.status(401).json({ success: false, message: "User not found" });
-            }
-
-            next()
-        }
-        catch(error){
-            return res.status(401).json({ success: false, message: "Unauthorized - Invalid Token" });
-        }
-    }
-    else{
-        return res.status(401).json({ success: false, message: "No token provided" });
-    }  
-}
+    next();
+  } catch (error) {
+    console.log("Error in authorisedRolesMiddleware", error);
+    res.status(500).json({
+      success: false,
+      message: "An error occurred while authorising roles.",
+      error: error.message,
+    });
+  }
+};
--- backend/src/routes/appointmentRoutes.js
+++ backend/src/routes/appointmentRoutes.js
@@ -1,18 +1,19 @@
 import express from "express";
 import { bookAppointment, rescheduleAppointment, cancelAppointment} from "../controllers/appointmentController.js";
-import { validateAuthenticationMiddleware } from "../middlewares/authMiddleware.js";
+import { authorisedRolesMiddleware, validateAuthenticationMiddleware } from "../middlewares/authMiddleware.js";
 import { validateBookAppointmentMiddleware, validateRescheduleAppointmentMiddleware } from "../middlewares/appointmentMiddleware.js";
+import { USER_TYPE } from "../constants/enums.js";
 
 const router = express.Router();
 
 // Book an appointment
-router.post("/", validateAuthenticationMiddleware, validateBookAppointmentMiddleware, bookAppointment);
+router.post("/", validateAuthenticationMiddleware, authorisedRolesMiddleware([USER_TYPE.PATIENT]), validateBookAppointmentMiddleware, bookAppointment);
 
 // Reschedule an appointment
-router.patch("/:appointmentId/reschedule", validateAuthenticationMiddleware, validateRescheduleAppointmentMiddleware, rescheduleAppointment);
+router.patch("/:appointmentId/reschedule", validateAuthenticationMiddleware, authorisedRolesMiddleware([USER_TYPE.PATIENT, USER_TYPE.DOCTOR]), validateRescheduleAppointmentMiddleware, rescheduleAppointment);
 
 // Cancel an appointment
-router.delete("/:appointmentId", validateAuthenticationMiddleware, cancelAppointment);
+router.delete("/:appointmentId", validateAuthenticationMiddleware, authorisedRolesMiddleware([USER_TYPE.PATIENT, USER_TYPE.DOCTOR]), cancelAppointment);
 
 /*
 router.get("/", validateAuthenticationMiddleware, getAppointments); 
