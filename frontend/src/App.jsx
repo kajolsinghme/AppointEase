ccd080f619275689849067dc412f17f8e3eebe0b
kajolsinghme <kajolsingh985@gmail.com>
2025-04-15 12:58:12 +0530

    Initialize React app with Vite and Tailwind CSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-pink-100">
+      <h1 className="text-5xl font-bold underline text-red-500">
+        Hey There!
+      </h1>
+    </div>
+  );
+}
+export default App;
