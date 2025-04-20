b5295c99b7c4c5a3328da3dd3cd557c472d44f30
kajolsinghme <kajolsingh985@gmail.com>
2025-04-20 23:59:10 +0530

    Refactored Search Bar Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import {BrowserRouter, Routes, Route}  from 'react-router-dom'
 import Home from './pages/Home';
+import ExploreDoctors from './pages/ExploreDoctors';
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/explore-doctors' element={<ExploreDoctors/>} />
       </Routes>
     </BrowserRouter>
   );
