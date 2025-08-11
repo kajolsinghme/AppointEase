3326624d1c9e9f6c597bab381482362421bd8dd2
kajolsinghme <kajolsingh985@gmail.com>
2025-08-11 20:23:51 +0530

    added health check message

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,22 +2,25 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import cors from "cors";
-import authRoutes from "./routes/authRoutes.js"
-import userRoutes from "./routes/userRoutes.js"
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import appointmentRoutes from "./routes/appointmentRoutes.js";
-import uploadRoutes from "./routes/upload.js"
+import uploadRoutes from "./routes/upload.js";
 
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Routes
-app.use("/api/auth", authRoutes); 
+app.get("/status", (req, res) => {
+  res.status(200).send("Backend running successfully");
+});
+app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
-app.use("/api/appointments", appointmentRoutes)
-app.use("/api", uploadRoutes)
+app.use("/api/appointments", appointmentRoutes);
+app.use("/api", uploadRoutes);
 
 export default app;
